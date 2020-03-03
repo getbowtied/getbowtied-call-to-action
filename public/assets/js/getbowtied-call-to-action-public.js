262d31d4038a12c113c19c9d18dcb23483310dc5
Vanesa Reinerth <vanesa@getbowtied.com>
2020-03-03 14:54:15 +0200

    fixes

--- public/assets/js/getbowtied-call-to-action-public.js
+++ public/assets/js/getbowtied-call-to-action-public.js
@@ -7,18 +7,30 @@ jQuery( function ($) {
 		data: {}
 	}).done( function( response ) {
 		if( response.data ) {
-			setTimeout( function() {
-				$('body').addClass('show-call-to-action-canvas');
-			}, 2000 );
+			$('body').addClass('show-call-to-action-canvas');
 		}
 	});
 
-	$('.call-to-action-toggle, .call-to-action-canvas-overlay, .call-to-action-close').on( 'click', function() {
+	$('.call-to-action-toggle, .call-to-action-close').on( 'click', function() {
 		$('body').toggleClass('show-call-to-action-canvas');
 
         $.post(call_to_action_vars.ajaxurl, {
             action: 'set_cookie',
             data: { 'is_canvas_open': $('body').hasClass('show-call-to-action-canvas') ? '1' : '0' }
         });
+    });
+
+	$('.call-to-action-latest-layout-image, .call-to-action-layout-image').on( 'click', function(e) {
+		e.preventDefault();
+
+		$('body').toggleClass('show-call-to-action-canvas');
+
+		$.post(call_to_action_vars.ajaxurl, {
+            action: 'set_cookie',
+            data: { 'is_canvas_open': $('body').hasClass('show-call-to-action-canvas') ? '1' : '0' }
+        });
+
+		var button = $(this);
+		setTimeout(function(){ $(location).attr( 'href', button.attr('href') ); }, 350);
     });
 });
